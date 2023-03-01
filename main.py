39e7e12f0fa2d626a738a74754562504321cfda9
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-03-01 16:00:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Libraries
+import streamlit as st
+from PIL import Image
+
+
+# Confit
+st.set_page_config(page_title='Lorem Ipsum', page_icon=':bar_chart:', layout='wide')
+
+# Title
+st.title('Lorem Ipsum')
